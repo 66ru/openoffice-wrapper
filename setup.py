ffc2dae80a85f8e1c56791016c16afe33c770b9d
Anton Romanovich <anthony.romanovich@gmail.com>
2012-09-05 13:10:50 +0600

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'openoffice-wrapper',
+    version = '0.1',
+
+    author = 'Anton Romanovich',
+    author_email = 'anthony.romanovich@gmail.com',
+    
+    py_modules=['oowrapper']
+)
